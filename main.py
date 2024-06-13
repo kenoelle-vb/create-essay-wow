5824e6dfc7f8c8a7ffed2f175d3b48310ac4d20c
kenoelle-vb <knolel9001@gmail.com>
2024-06-13 12:58:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from langchain_community.document_loaders import TextLoader
 import streamlit as st
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.embeddings.sentence_transformer import SentenceTransformerEmbeddings
+from langchain_community.embeddings import FastEmbedEmbeddings
+from langchain_community.vectorstores import Chroma
 from langchain_groq import ChatGroq
 
 loader = TextLoader("dummy.txt")
@@ -14,4 +16,8 @@ st.write(chunks[0])
 
 llm = ChatGroq(groq_api_key="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", model_name="llama3-8b-8192")
 
-embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+#embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+
+documents = chunks
+embeddings = FastEmbedEmbeddings()
+vector_store = Chroma.from_documents(documents, embeddings)
