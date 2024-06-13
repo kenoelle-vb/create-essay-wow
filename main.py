c1b59a2ba5b3d0b2b9f9d0a187dd5a75412ece2a
kenoelle-vb <knolel9001@gmail.com>
2024-06-13 12:52:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ import streamlit as st
 loader = TextLoader("dummy.txt")
 data = loader.load()
 st.write(data)
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=4000, chunk_overlap=1000)
+chunks = text_splitter.split_documents(data)
+st.write(chunks[0])
