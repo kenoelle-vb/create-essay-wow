6b95cf6410fa0380d8fa2fab9de8723037f24ce6
kenoelle-vb <knolel9001@gmail.com>
2024-06-13 13:24:13 +0700

    Update main.py

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ llm = ChatGroq(groq_api_key="gsk_oWevZ32OOyaupynRZG7iWGdyb3FYMhg1yUw3bwkjfbttS5H
 number = 2
 
 title = st.text_input("")
+st.write(len(chunks))
 client = Groq(api_key="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")
 summary= f"Answer the question from {title}, only answer from {chunks[number]}"
 final = client.chat.completions.create(messages=[{"role":"user", "content":summary,}],model="llama3-8b-8192")
